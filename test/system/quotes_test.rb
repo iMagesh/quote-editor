078bef37629e822b6451a49cae1e8749632323ee
Magesh <pingmagesh@gmail.com>
2025-09-09 17:52:03 -0400

    updated test to suit the turbo frame and stream implementation

--- test/system/quotes_test.rb
+++ test/system/quotes_test.rb
@@ -3,7 +3,7 @@ require "application_system_test_case"
 class QuotesTest < ApplicationSystemTestCase
 
   setup do
-    @quote = quotes(:first) # Reference to the first fixture quote
+    @quote = Quote.ordered.first # Reference to the first fixture quote
   end
   
   test "Creating a new quote" do
@@ -15,11 +15,12 @@ class QuotesTest < ApplicationSystemTestCase
     # When we click on the link with the text "New quote"
     # we expect to land on a page with the title "New quote"
     click_on "New Quote"
-    assert_selector "h1", text: "New quote"
 
     # When we fill in the name input with "Capybara quote"
     # and we click on "Create Quote"
     fill_in "Name", with: "Capybara quote"
+    
+    assert_selector "h1", text: "Quotes"
     click_on "Create quote"
 
     # We expect to be back on the page with the title "Quotes"
@@ -40,9 +41,10 @@ class QuotesTest < ApplicationSystemTestCase
     assert_selector "h1", text: "Quotes"
 
     click_on "Edit", match: :first
-    assert_selector "h1", text: "Edit quote"
 
     fill_in "Name", with: "Updated quote"
+
+    assert_selector "h1", text: "Quotes"
     click_on "Update quote"
 
     assert_selector "h1", text: "Quotes"
